bb6ffe991078a02d6b15fc87488d14055064d91e
SamReinsteinWright <thelittlepixel123@gmail.com>
2023-09-08 15:30:53 -0400

    Created the activity#2

---
+++ experiments/Activity#2/template-p5-project/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**************************************************
+Activity #2 - Drawing an Alien
+Sam Reinstein Wright
+
+I guess i gotta draw an alien or smthn, not too sure 
+what kind of alien or even if it matters
+(I havent watched the video with the details)
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+    // created canvas and background
+    createCanvas(500,500);
+    background(200,200,200);
+
+
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
