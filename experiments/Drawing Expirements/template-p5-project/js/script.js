aa22fb09b067fda316d10e2d0cb6612e90447470
SamReinsteinWright <thelittlepixel123@gmail.com>
2023-09-08 15:15:21 -0400

    learned how to comment on code

--- experiments/Drawing Expirements/template-p5-project/js/script.js
+++ experiments/Drawing Expirements/template-p5-project/js/script.js
@@ -12,12 +12,15 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
 createCanvas(500,500);
-
+// made background grey
 background(200,200,200);
-
+// centered all rectangles
 rectMode(CENTER);
+// removed rectangle border
 noStroke();
+// set rectangle colour
 fill(255,100,38);
+// drew rectangle
 rect(250,250,100,50);
 
 
