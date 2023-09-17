447511feb8ddadfa275789db526ff212775d9a2e
SamReinsteinWright <thelittlepixel123@gmail.com>
2023-09-17 13:53:04 -0400

    made an angry man, that fulfills most of the requirements for the project

--- experiments/i-like-to-move-it/Variables/js/script.js
+++ experiments/i-like-to-move-it/Variables/js/script.js
@@ -12,6 +12,17 @@
  *  for preloading images and things so 
  * they come in fully downloaded
 */
+
+let rectSize;
+
+let absoluteCenterX;
+
+let absoluteCenterY;
+
+let mouseDistance;
+
+
+
 function preload() {
 
 }
@@ -22,6 +33,9 @@ function preload() {
 */
 function setup() {
 createCanvas(windowWidth,windowHeight);
+rectSize = 100;
+absoluteCenterX = (width/2);
+absoluteCenterY = (height/2);
 
 
 
@@ -33,7 +47,18 @@ createCanvas(windowWidth,windowHeight);
  * makin shit!
 */
 function draw() {
-    background(200,200,200);
+    mouseDistance = (sqrt(sq(absoluteCenterX-mouseX)+sq(absoluteCenterY-mouseY)));
+    background(mouseDistance,0,0);
     rectMode(CENTER);
     rect(mouseX,mouseY,100,100);
+    ellipse(absoluteCenterX,absoluteCenterY,200,200);
+    ellipse(absoluteCenterX+30,absoluteCenterY-20, 50, mouseDistance);
+    ellipse(absoluteCenterX-30,absoluteCenterY-20, 50,mouseDistance);
+    ellipse(absoluteCenterX,absoluteCenterY+50, 50,mouseDistance);
+    strokeWeight(5);
+    line(absoluteCenterX+50,(absoluteCenterX-mouseDistance)+(absoluteCenterX/50)-75,absoluteCenterX+20,(absoluteCenterX-mouseDistance/2)+(absoluteCenterX/25)-75);
+    line(absoluteCenterX-50,(absoluteCenterX-mouseDistance)+(absoluteCenterX/50)-75,absoluteCenterX-20,(absoluteCenterX-mouseDistance/2)+(absoluteCenterX/25)-75);
+
+
+
 }
